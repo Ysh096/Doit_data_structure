5cfbfa464e49220c87e8059277672fd0a1c37056
Seongho <skk7541@gmail.com>
2021-03-04 04:07:15 +0900

    image error correction

--- 3. 검색 알고리즘 이론&문제/완주하지못한선수_sort.py
+++
@@ -1,6 +0,0 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for i in range(len(participant)):
-        if participant[i] != completion[i]:
-            return participant[i]
